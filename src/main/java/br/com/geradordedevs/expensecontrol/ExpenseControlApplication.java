ee2de2463705d15ad1885df836607bc5dc8525b1
Mateus Ferian <mateusferian10@gmail.com>
2023-01-16 09:54:56 -0300

    :tada: commit initial

---
+++ src/main/java/br/com/geradordedevs/expensecontrol/ExpenseControlApplication.java
@@ -0,0 +1,13 @@
+package br.com.geradordedevs.expensecontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/geradordedevs/expensecontrol/ExpenseControlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.geradordedevs.expensecontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
