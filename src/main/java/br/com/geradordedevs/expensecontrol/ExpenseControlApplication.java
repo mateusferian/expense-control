ad799865e11e63cb5c3664721f2c5ed0027d1694
Mateus Ferian <mateusferian10@gmail.com>
2023-01-19 18:08:08 -0300

    :recycle: refactor: readjusting

--- src/main/java/br/com/geradordedevs/expensecontrol/ExpenseControlApplication.java
+++ src/main/java/br/com/geradordedevs/expensecontrol/ExpenseControlApplication.java
@@ -1,5 +1,6 @@
 package br.com.geradordedevs.expensecontrol;
 
+
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
