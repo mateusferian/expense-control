4453fe163f26936021a6f5e4bf6ea026b7e420f6
mateusferian <mateusferian10@gmail.com>
2023-01-24 21:10:42 -0300

    :white_check_mark: test: creating unit tests for controllers layer uploadCustomersData method

--- src/test/java/br/com/geradordedevs/expensecontrol/controllers/SpreadsheetControllerTest.java
+++ src/test/java/br/com/geradordedevs/expensecontrol/controllers/SpreadsheetControllerTest.java
@@ -7,12 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -30,9 +30,17 @@ public class SpreadsheetControllerTest {
 
     private final String ROULE_SPREADSHEET = "/api/v1/expenses-controls";
 
+    private final String ROULE_SPREADSHEETA_UPLOAD = "/api/v1/expenses-controls/upload-spreadsheet-data";
+
     @Test
     public  void findAllSpreadsheetReturnOk() throws Exception{
         mockMvc.perform(get(ROULE_SPREADSHEET))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    public void uploadCustomersDataReturnCreated() throws Exception {
+        MockMultipartFile mmf = new MockMultipartFile("file", "test.xlsx", "text/plain", "testesasa".getBytes());
+        mockMvc.perform(multipart(ROULE_SPREADSHEETA_UPLOAD).file(mmf)).andExpect(status().isCreated());
     }
 }
